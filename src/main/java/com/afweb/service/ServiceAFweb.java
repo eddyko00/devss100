2cbfd61e83db554d519021e2ce670c8a73b819aa
koed <koed@L097105.corp.ads>
2020-04-25 10:50:06 -0400

    ver1

--- src/main/java/com/afweb/service/ServiceAFweb.java
+++ src/main/java/com/afweb/service/ServiceAFweb.java
@@ -399,7 +399,7 @@ public class ServiceAFweb {
                         }
                     }
 
-                    boolean restoreSsnsAccFlag = false; // work for remote d
+                    boolean restoreSsnsAccFlag = true; // work for remote d
                     if (restoreSsnsAccFlag == true) {
                         this.getSsnsDataImp().deleteAllSsnsAcc(0);
                         boolean retSatus = getAccountImp().restoreSsnsAccDB(this);
